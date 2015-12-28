e9fa1cc2ac19ea61548488ef18d36bacd09f4cf9
Praveen Gattu <pgattu@gmail.com>
2015-12-28 16:48:06 -0700

    updated

--- gitpush.sh
+++ gitpush.sh
@@ -5,13 +5,12 @@ WEB_HOME=~/www
 #git add $WEB_HOME/css
 #git add $WEB_HOME/images
 
-git add *.php
-git add js/
-git add css/
-git add images/
+git add $WEB_HOME/*.php
+git add $WEB_HOME/js/
+git add $WEB_HOME/css/
+git add $WEB_HOME/images/
 
 git commit -m "`date`"
 
 # git remote add origin https://github.com/StrawberrySystems/ss-web.git
-git push -u origin master
-
+git push --set-upstream --force origin master
